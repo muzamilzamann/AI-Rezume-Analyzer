407291dd52c1ca68a39771b9018aec6cb22fb685
Muzamil Zaman <your-email@example.com>
2026-07-17 10:58:44 +0500

    Update 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from '@/context/AuthContext'
 import App from './App.tsx'
 import './index.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
